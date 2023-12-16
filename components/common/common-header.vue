62c9b905fe03af2b7a0c9b7ee9a29e6da2a04189
妖雨纯 <876451336@qq.com>
2023-12-16 23:36:41 +0800

    ✨ feat(header): 新增github地址连接

--- components/common/common-header.vue
+++ components/common/common-header.vue
@@ -10,15 +10,21 @@ export default defineComponent({
         return () => (
             <n-layout-header class="common-header">
                 <n-element class="common-header__container n-chunk n-center">
-                    <n-space size={24} wrap-item={false} align="center" style={{ width: '100%' }}>
+                    <n-space size={20} wrap-item={false} align="center" justify="space-between" style={{ width: '100%' }}>
                         <n-button text focusable={false}>
-                            <common-wrapper size={48} name="Nuxt"></common-wrapper>
-
-                            <n-h1 style={{ margin: 0, fontSize: '26px', fontWeight: 600, padding: '12px 10px 0' }}>Document</n-h1>
+                            <common-wrapper size={40} name="Nuxt"></common-wrapper>
+                            <n-h2 style={{ margin: 0, fontWeight: 600, padding: '8px 5px 0' }}>Document</n-h2>
                         </n-button>
-                        <n-button text focusable={false} onClick={(e: Event) => setTheme(inverted.value ? 'light' : 'dark')}>
-                            <common-wrapper size={24} name={inverted.value ? 'ThemeDark' : 'ThemeLight'}></common-wrapper>
-                        </n-button>
+                        <n-space size={16} wrap-item={false} align="center">
+                            <n-button text focusable={false} onClick={(e: Event) => setTheme(inverted.value ? 'light' : 'dark')}>
+                                <common-wrapper size={28} name={inverted.value ? 'ThemeDark' : 'ThemeLight'}></common-wrapper>
+                            </n-button>
+                            <n-a type="default" target="_blank" href="https://github.com/Wlisfes/nest-basic-document">
+                                <n-button text focusable={false}>
+                                    <common-wrapper size={28} name="Github"></common-wrapper>
+                                </n-button>
+                            </n-a>
+                        </n-space>
                     </n-space>
                 </n-element>
             </n-layout-header>
