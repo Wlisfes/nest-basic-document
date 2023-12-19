d4f445b4c11fef7e7e045bf87e3f963f6fff8803
妖雨纯 <876451336@qq.com>
2023-12-19 22:45:59 +0800

    ✨ feat(server): 新增user相关接口

--- server/typeorm/entity/table.user.ts
+++ server/typeorm/entity/table.user.ts
@@ -8,4 +8,21 @@ export class TableUser extends TableCommon {
 
     @Column({ type: 'varchar', comment: '昵称', nullable: false })
     nickname: string
+
+    @Column({ type: 'varchar', comment: '邮箱', nullable: true })
+    email: string
+
+    @Column({ type: 'varchar', comment: '头像', nullable: true })
+    avatar: string
+
+    @Column({ type: 'varchar', comment: '手机号', nullable: false })
+    mobile: string
+
+    @Column({
+        type: 'varchar',
+        comment: '密码',
+        select: false,
+        nullable: false
+    })
+    password: string
 }
