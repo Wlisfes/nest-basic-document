4a740912774de88bee5ce0196848c173032b6d14
妖雨纯 <876451336@qq.com>
2023-12-22 19:13:31 +0800

    ✨ feat(server): 新增参数验证插件

--- server/api/user/register.post.ts
+++ server/api/user/register.post.ts
@@ -1,7 +1,32 @@
 import { createBaser } from '@/server/typeorm'
 import { TableUser } from '@/server/typeorm/entity'
+import { validate, validateOrReject, IsNotEmpty, Length } from 'class-validator'
+import { Exclude } from 'class-transformer'
+
+export class Register {
+    @IsNotEmpty({ message: '密码 必填' })
+    @Length(6, 18, { message: '密码格式错误' })
+    password: string
+}
 
 export default defineEventHandler(async event => {
+    const body = await readBody(event)
+    const post = new TableUser()
+    // post.password = body.password
+    post.setUid(body.password)
+    validateOrReject(post).catch(errors => {
+        console.log('🤧🤧🤧🤧 Promise rejected (validation failed). Errors: ', errors)
+    })
+
+    // 如果验证失败不会停止运行程序
+    validate(post).then(errors => {
+        if (errors.length > 0) {
+            console.log('😈😈😈😈 validation failed. errors: ', errors)
+        } else {
+            console.log('validation succeed')
+        }
+    })
+
     return await createBaser(event.context.db, TableUser).then(async model => {
         return {
             code: 200,
--- server/typeorm/entity/table.user.ts
+++ server/typeorm/entity/table.user.ts
@@ -1,23 +1,31 @@
 import { Entity, Column } from 'typeorm'
+import { IsNotEmpty, Length, IsEmail } from 'class-validator'
 import { TableCommon } from '@/server/typeorm/entity/table.common'
 
 @Entity('table_user')
 export class TableUser extends TableCommon {
+    @IsNotEmpty({ message: 'UID 必填' })
     @Column({ type: 'varchar', comment: 'uid', update: false, nullable: false })
     uid: string
 
+    @IsNotEmpty({ message: '用户昵称 必填' })
     @Column({ type: 'varchar', comment: '昵称', nullable: false })
     nickname: string
 
+    @IsNotEmpty({ message: '邮箱 必填' })
+    @IsEmail({}, { message: '邮箱 格式错误' })
     @Column({ type: 'varchar', comment: '邮箱', nullable: true })
     email: string
 
     @Column({ type: 'varchar', comment: '头像', nullable: true })
+    @IsNotEmpty({ message: '头像 必填' })
     avatar: string
 
     @Column({ type: 'varchar', comment: '手机号', nullable: false })
     mobile: string
 
+    @IsNotEmpty({ message: '密码 必填' })
+    @Length(6, 18, { message: '密码格式错误' })
     @Column({
         type: 'varchar',
         comment: '密码',
@@ -25,4 +33,8 @@ export class TableUser extends TableCommon {
         nullable: false
     })
     password: string
+
+    setUid(uid: string) {
+        this.uid = uid
+    }
 }
