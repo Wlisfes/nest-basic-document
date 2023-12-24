a2a2b15f82d480a152aed7c51f6ffa2cfb74a5f0
妖雨纯 <876451336@qq.com>
2023-12-24 20:55:59 +0800

    ✨ feat(Base): 新增登录、注册界面

---
+++ pages/middle/authorize.vue
@@ -0,0 +1,11 @@
+<script lang="tsx">
+export default defineNuxtComponent({
+    name: 'Authorize',
+    head: () => ({
+        titleTemplate: (title: string) => `${title} - 登录`
+    }),
+    setup() {
+        return () => <n-element class="layout-provider n-chunk n-column n-center n-auto no-selecter"></n-element>
+    }
+})
+</script>
---
+++ pages/middle/register.vue
@@ -0,0 +1,11 @@
+<script lang="tsx">
+export default defineNuxtComponent({
+    name: 'Register',
+    head: () => ({
+        titleTemplate: (title: string) => `${title} - 注册`
+    }),
+    setup() {
+        return () => <n-element class="layout-provider n-chunk n-column n-center n-auto no-selecter"></n-element>
+    }
+})
+</script>
