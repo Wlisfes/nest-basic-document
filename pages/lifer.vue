9f737b3a6135dc0a30903c03a1cdd86dca29a5a0
妖雨纯 <876451336@qq.com>
2023-12-24 13:15:34 +0800

    ✨ feat(Base): 新增基础页面组件

---
+++ pages/lifer.vue
@@ -0,0 +1,10 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Lifer',
+    setup(props, { slots }) {
+        return () => <div>lifer</div>
+    }
+})
+</script>
---
+++ pages/star.vue
@@ -0,0 +1,10 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Star',
+    setup(props, { slots }) {
+        return () => <div>star</div>
+    }
+})
+</script>
