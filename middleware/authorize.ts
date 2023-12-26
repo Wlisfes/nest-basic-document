fd2fb840bd25e1bb3540e80101c0179fd1b8b02e
妖雨纯 <876451336@qq.com>
2023-12-26 23:51:13 +0800

    ✨ feat(middleware): 数据捕获错误存储地址

--- middleware/authorize.ts
+++ middleware/authorize.ts
@@ -1,9 +1,11 @@
 import { useUser } from '@/store/user'
 import { divineHandler } from '@/utils/utils-common'
+import { APP_NUXT, setStore } from '@/utils/utils-cookie'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const token = getToken()
     if (process.client && !token) {
+        await setStore(APP_NUXT.APP_NUXT_REDIRECT, to.fullPath)
         return await navigateTo(`/common/authorize`)
     } else if (process.client) {
         const store = useUser()
@@ -11,6 +13,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
             try {
                 await store.fetchUserResolver()
             } catch (err) {
+                await setStore(APP_NUXT.APP_NUXT_REDIRECT, to.fullPath)
                 return await navigateTo('/')
             }
         })
--- middleware/resolver.ts
+++ middleware/resolver.ts
@@ -1,5 +1,6 @@
 import { useUser } from '@/store/user'
 import { divineHandler } from '@/utils/utils-common'
+import { APP_NUXT, setStore } from '@/utils/utils-cookie'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const token = getToken()
@@ -9,6 +10,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
             try {
                 await store.fetchUserResolver()
             } catch (err) {
+                await setStore(APP_NUXT.APP_NUXT_REDIRECT, to.fullPath)
                 return await navigateTo('/')
             }
         })
