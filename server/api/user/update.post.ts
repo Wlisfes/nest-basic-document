8491d3f018fdd5a356986bb7c1f9dfe72e747474
妖雨纯 <876451336@qq.com>
2023-12-30 19:42:53 +0800

    ✨ feat(server): 临时存储

--- server/api/user/update.post.ts
+++ server/api/user/update.post.ts
@@ -2,13 +2,7 @@ import { createBaser } from '@/server/typeorm'
 import { TableUser } from '@/server/typeorm/database'
 
 export default defineEventHandler(async event => {
-    const query = getQuery(event)
-    console.log(query)
-    return await createBaser(event.context.db, TableUser).then(async model => {
-        return {
-            query,
-            code: 200,
-            list: model.find()
-        }
+    return divineEventCatcher(event, async evt => {
+        return { message: '修改成功' }
     })
 })
