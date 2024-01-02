95ed9e1fb38dc2a19ac5f4054db948fb3d0b2bc7
妖雨纯 <876451336@qq.com>
2024-01-03 00:08:05 +0800

    🌈 style(Home): 临时存储

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 export default defineNuxtComponent({
     name: 'Index',
     async setup() {
-        await createSeoMeta('首页')
+        await createSeoMeta('一个神奇的网站')
         return () => (
             <n-element class="layout-provider n-chunk n-column n-center n-auto no-selecter">
                 <common-clock max={960}></common-clock>
