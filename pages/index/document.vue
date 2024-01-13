2345a4becbb56b1b2cdb57e0ed407e3cbe0bebcb
妖雨纯 <876451336@qq.com>
2024-01-13 13:25:32 +0800

    🐎 ci(config): 新增md-editor-v3依赖包

--- pages/index/document.vue
+++ pages/index/document.vue
@@ -2,13 +2,7 @@
 export default defineNuxtComponent({
     name: 'Document',
     setup(props, { slots }) {
-        return () => (
-            <n-element class="layout-provider n-chunk n-column n-center n-auto no-selecter">
-                {Array.from({ length: 20 }, (x, i) => (
-                    <n-h1 key={i}>{`Document ${i}`}</n-h1>
-                ))}
-            </n-element>
-        )
+        return () => <n-element class="layout-provider">Document</n-element>
     }
 })
 </script>
