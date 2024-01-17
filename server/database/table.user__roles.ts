3dce734d7271f71310229b6804cfeb1ece0e74db
妖雨纯 <876451336@qq.com>
2024-01-17 19:29:18 +0800

    ✨ feat(server): 完善用户角色表结构

--- server/database/table.user__roles.ts
+++ server/database/table.user__roles.ts
@@ -3,4 +3,12 @@ import { IsNotEmpty, Length, IsEmail, isEmpty } from 'class-validator'
 import { TableCommon } from '@/server/database/table.common'
 
 @Entity('table_user__roles')
-export class TableUserRoles extends TableCommon {}
+export class TableUserRoles extends TableCommon {
+    @IsNotEmpty({ message: '角色标识 必填', groups: ['key'] })
+    @Column({ type: 'varchar', comment: '角色标识', nullable: false })
+    key: string
+
+    @IsNotEmpty({ message: '角色名称 必填', groups: ['name'] })
+    @Column({ type: 'varchar', comment: '角色名称', nullable: false })
+    name: string
+}
