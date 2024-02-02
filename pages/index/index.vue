a627903155f9fd6e20c59a2935d7364d30e7880d
妖雨纯 <876451336@qq.com>
2024-02-02 11:35:12 +0800

    ✨ feat(index): 首页seo优化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,8 +2,10 @@
 export default defineNuxtComponent({
     name: 'Index',
     setup() {
+        const config = useRuntimeConfig()
         return () => (
             <n-element class="layout-provider n-chunk n-column n-center n-auto no-selecter">
+                <div style={{ display: 'none' }}>{config.public.APP_NUXT_DESCRIPTIOM}</div>
                 <common-clock max={960}></common-clock>
             </n-element>
         )
