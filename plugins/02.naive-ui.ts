2e02850bcd5d73aea4f092d875ae984084663f45
妖雨纯 <876451336@qq.com>
2025-05-20 19:05:10 +0800

    ✨ feat(plugins): 优化首屏样式

--- plugins/02.naive-ui.ts
+++ plugins/02.naive-ui.ts
@@ -4,31 +4,19 @@ import { defineNuxtPlugin } from '#app'
 export default defineNuxtPlugin(nuxtApp => {
     if (process.server) {
         const { collect } = setup(nuxtApp.vueApp)
-        const originalRenderMeta = nuxtApp.ssrContext?.renderMeta
-        // @ts-ignore
-        nuxtApp.ssrContext = nuxtApp.ssrContext || {}
-        // @ts-ignore
-        nuxtApp.ssrContext.renderMeta = () => {
-            if (!originalRenderMeta) {
-                return {
-                    headTags: collect()
-                }
-            }
-            const originalMeta = originalRenderMeta()
-            if ('then' in originalMeta) {
-                // @ts-ignore
-                return originalMeta.then(resolvedOriginalMeta => {
-                    return {
-                        ...resolvedOriginalMeta,
-                        headTags: resolvedOriginalMeta['headTags'] + collect()
+        useServerHead({
+            style: () => {
+                const stylesString = collect()
+                const stylesArray = stylesString.split(/<\/style>/g).filter(style => style)
+                return stylesArray.map((styleString: string) => {
+                    const match = styleString.match(/<style cssr-id="([^"]*)">([\s\S]*)/)
+                    if (match) {
+                        const id = match[1]
+                        return { 'cssr-id': id, children: match[2] }
                     }
+                    return {}
                 })
-            } else {
-                return {
-                    ...originalMeta,
-                    headTags: originalMeta['headTags'] + collect()
-                }
             }
-        }
+        })
     }
 })
